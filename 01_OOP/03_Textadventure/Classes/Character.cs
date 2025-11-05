027f9f059097ca48f6a320c6270d56bf4402472d
Milowiz <tpamilowiz@gmail.com>
2025-11-05 15:59:40 +0100

    Fix

--- 01_OOP/03_Textadventure/Classes/Character.cs
+++ 01_OOP/03_Textadventure/Classes/Character.cs
@@ -6,12 +6,25 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
-    public static class Character
+    public class Character
     {
-
-        // public Game{
-        //     True = True;
-        // }
+        ushort MaxLives { get; set; }
+        ushort CurrentLives { get; set; }
+        string[] Inventory { get; set; }
+        ushort Damage { get; set; }
+        string Name { get; set; }
+        string DamageType{ get; set; }
+        string DefenseType { get; set; }
+        public Character(ushort maxLives, ushort currentLives, string[] inventory, ushort damage, string name, string damageType, string defenseType)
+        {
+            MaxLives = maxLives;
+            CurrentLives = currentLives;
+            Inventory = inventory;
+            Damage = damage;
+            Name = name;
+            DamageType = damageType;
+            DefenseType = defenseType;
+        }
         public static void DoIt()
         {
             System.Console.WriteLine("Player");
--- 01_OOP/03_Textadventure/Classes/PlayerCharacter.cs
+++ 01_OOP/03_Textadventure/Classes/PlayerCharacter.cs
@@ -1,12 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Classes;
 
 namespace _03_Textadventure.Classes
 {
-    public class PlayerCharacter
+    public class PlayerCharacter : Character
     {
-        
+        public PlayerCharacter(ushort maxLives, ushort currentLives, string[] inventory, ushort damage, string name, string damageType, string defenseType)
+        : base(maxLives, currentLives, inventory, damage, name, damageType, defenseType)
+        {
+
+        }
+       
+       public void Test()
+        {
+            
+       }
     }
 }
